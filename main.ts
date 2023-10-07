e8aa79e64d6091897393db38a60c2e167fcecb6a
DLN-001 <77555673+DLN-001@users.noreply.github.com>
2023-10-06 23:01:41 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -39,63 +39,69 @@ function initializeHeroHealth () {
     HP.setColor(7, 2)
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    IsMovingDragonHead = !(IsMovingDragonHead)
-    for (let index = 0; index <= custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`); index++) {
-        Dragon_Wing_Front.setImage(custom.getFrame(assets.animation`Dragon_Wing_Flap`, index))
-        pause(10)
-    }
-    for (let index = 0; index <= custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`); index++) {
-        Dragon_Wing_Front.setImage(custom.getFrame(assets.animation`Dragon_Wing_Flap`, custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`) - index))
-        pause(10)
-    }
+    moveDragonLegPair(Dragon_Leg_Back1, Dragon_Leg_Front1, true)
+    pause(100)
+    moveDragonLegPair(Dragon_Leg_Back2, Dragon_Leg_Front2, true)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     shootDragonFire()
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveDragonLegPair(Dragon_Leg_Back1, Dragon_Leg_Front1, true)
-    pause(100)
-    moveDragonLegPair(Dragon_Leg_Back2, Dragon_Leg_Front2, true)
+    HeroIsForward = false
+    while (HeroCameraXOffset > -50 && HeroIsForward == false) {
+        HeroCameraXOffset += -1
+        pause(10)
+    }
 })
 function test4 () {
+    Dragon_Neck_Col = 13
+    Dragon_Neck_Row = 12
     Dragon_Neck = sprites.create(custom.getFrame(assets.animation`Dragon_Neck`, 0), SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Neck, tiles.getTileLocation(8, 12))
+    tiles.placeOnTile(Dragon_Neck, tiles.getTileLocation(Dragon_Neck_Col, Dragon_Neck_Row))
     Dragon_Neck.y += 8
     Dragon_Head = sprites.create(custom.getFrame(assets.animation`Dragon_Head`, 0), SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Head, tiles.getTileLocation(6, 10))
+    tiles.placeOnTile(Dragon_Head, tiles.getTileLocation(Dragon_Neck_Col - 2, Dragon_Neck_Row - 2))
     Dragon_Head.x += 10
     Dragon_Head.y += 24
     Dragon_Body = sprites.create(assets.image`Dragon_Body`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Body, tiles.getTileLocation(11, 13))
+    tiles.placeOnTile(Dragon_Body, tiles.getTileLocation(Dragon_Neck_Col + 3, Dragon_Neck_Row + 1))
     Dragon_Body.x += 0
     Dragon_Body.y += -6
     Dragon_Leg_Front1 = sprites.create(assets.image`Dragon_Leg_Front`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Leg_Front1, tiles.getTileLocation(9, 14))
+    tiles.placeOnTile(Dragon_Leg_Front1, tiles.getTileLocation(Dragon_Neck_Col + 1, Dragon_Neck_Row + 2))
     Dragon_Leg_Front1.y += -5
-    Dragon_Leg_Back1 = sprites.create(assets.image`Dragon_Leg_Back`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Leg_Back1, tiles.getTileLocation(12, 14))
-    Dragon_Leg_Back1.y += -5
     Dragon_Leg_Front2 = sprites.create(assets.image`Dragon_Leg_Front`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Leg_Front2, tiles.getTileLocation(9, 14))
+    tiles.placeOnTile(Dragon_Leg_Front2, tiles.getTileLocation(Dragon_Neck_Col + 1, Dragon_Neck_Row + 2))
     Dragon_Leg_Front2.y += -5
     Dragon_Leg_Front2.z += -10
+    Dragon_Leg_Back1 = sprites.create(assets.image`Dragon_Leg_Back`, SpriteKind.Test)
+    tiles.placeOnTile(Dragon_Leg_Back1, tiles.getTileLocation(Dragon_Neck_Col + 4, Dragon_Neck_Row + 2))
+    Dragon_Leg_Back1.y += -5
     Dragon_Leg_Back2 = sprites.create(assets.image`Dragon_Leg_Back`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Leg_Back2, tiles.getTileLocation(12, 14))
+    tiles.placeOnTile(Dragon_Leg_Back2, tiles.getTileLocation(Dragon_Neck_Col + 4, Dragon_Neck_Row + 2))
     Dragon_Leg_Back2.y += -5
     Dragon_Leg_Back2.z += -10
     Dragon_Wing_Front = sprites.create(assets.image`Dragon_Wing`, SpriteKind.Test)
-    tiles.placeOnTile(Dragon_Wing_Front, tiles.getTileLocation(11, 11))
+    tiles.placeOnTile(Dragon_Wing_Front, tiles.getTileLocation(Dragon_Neck_Col + 3, Dragon_Neck_Row - 1))
     Dragon_Wing_Front.y += 10
     Dragon_Wing_Front.x += -9
 }
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    HeroIsForward = false
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    HeroIsForward = true
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Dragon_Fire_Kind, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
     sprite.startEffect(effects.fire, 500)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveDragonLegPair(Dragon_Leg_Back1, Dragon_Leg_Front1, false)
-    pause(100)
-    moveDragonLegPair(Dragon_Leg_Back2, Dragon_Leg_Front2, false)
+    HeroIsForward = true
+    while (HeroCameraXOffset < 50 && HeroIsForward == true) {
+        HeroCameraXOffset += 1
+        pause(10)
+    }
 })
 function initializeHeroPower () {
     PP = statusbars.create(4, 20, StatusBarKind.Magic)
@@ -123,30 +129,33 @@ function moveDragonLegPair (myFirstLegSprite: Sprite, mySecondLegSprite: Sprite,
     moveDragonLeg(myFirstLegSprite, myForwardIndicator)
     pause(100)
     moveDragonLeg(mySecondLegSprite, myForwardIndicator)
+    scene.cameraShake(2, 100)
+    music.play(music.melodyPlayable(music.knock), music.PlaybackMode.UntilDone)
     if (myForwardIndicator) {
-        Dragon_Body.x += -3
-        Dragon_Neck.x += -3
-        Dragon_Head.x += -3
-        Dragon_Wing_Front.x += -3
+        Dragon_Body.x += -6
+        Dragon_Neck.x += -6
+        Dragon_Head.x += -6
+        Dragon_Wing_Front.x += -6
     } else {
-        Dragon_Body.x += 3
-        Dragon_Neck.x += 3
-        Dragon_Head.x += 3
-        Dragon_Wing_Front.x += 3
+        Dragon_Body.x += 6
+        Dragon_Neck.x += 6
+        Dragon_Head.x += 6
+        Dragon_Wing_Front.x += 6
     }
 }
 function moveDragonLeg (myLegSprite: Sprite, myForwardIndicator: boolean) {
     if (myForwardIndicator) {
         myLegSprite.y += -4
-        myLegSprite.x += -6
+        myLegSprite.x += -12
         pause(100)
         myLegSprite.y += 4
     } else {
         myLegSprite.y += -4
-        myLegSprite.x += 6
+        myLegSprite.x += 12
         pause(100)
         myLegSprite.y += 4
     }
+    pause(10)
 }
 function shootDragonFire () {
     if (Dragon_Mouth_Index == custom.getMaxFrameIndex(assets.animation`Dragon_Head`)) {
@@ -163,14 +172,28 @@ function shootDragonFire () {
         }
     }
 }
+function dragon_flag_wings () {
+    for (let index = 0; index <= custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`); index++) {
+        Dragon_Wing_Front.setImage(custom.getFrame(assets.animation`Dragon_Wing_Flap`, index))
+        pause(10)
+    }
+    Hero.ax = -50
+    for (let index = 0; index <= custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`); index++) {
+        Dragon_Wing_Front.setImage(custom.getFrame(assets.animation`Dragon_Wing_Flap`, custom.getMaxFrameIndex(assets.animation`Dragon_Wing_Flap`) - index))
+        pause(10)
+    }
+    Hero.ax = 0
+}
 let Dragon_Fire: Sprite = null
 let PP: StatusBarSprite = null
+let Dragon_Wing_Front: Sprite = null
 let Dragon_Body: Sprite = null
+let Dragon_Neck_Row = 0
+let Dragon_Neck_Col = 0
 let Dragon_Leg_Front2: Sprite = null
 let Dragon_Leg_Back2: Sprite = null
 let Dragon_Leg_Front1: Sprite = null
 let Dragon_Leg_Back1: Sprite = null
-let Dragon_Wing_Front: Sprite = null
 let HP: StatusBarSprite = null
 let Dragon_Neck: Sprite = null
 let Dragon_Head: Sprite = null
@@ -183,6 +206,8 @@ let Hero: Sprite = null
 let Dragon_Mouth_Index = 0
 let Dragon_Neck_Index = 0
 let IsMovingDragonHead = false
+let HeroCameraXOffset = 0
+HeroCameraXOffset = 50
 IsMovingDragonHead = true
 Dragon_Neck_Index = 0
 Dragon_Mouth_Index = 0
@@ -191,9 +216,18 @@ initializeHeroVariables()
 info.setScore(0)
 scene.setBackgroundImage(assets.image`Forest`)
 tiles.setCurrentTilemap(tilemap`level1`)
-tiles.placeOnTile(Hero, tiles.getTileLocation(10, 13))
+tiles.placeOnTile(Hero, tiles.getTileLocation(2, 13))
 initializeHeroHealth()
 initializeHeroPower()
 test4()
-scene.cameraFollowSprite(Hero)
 Hero.ay = 500
+controller.moveSprite(Hero, 100, 0)
+game.onUpdate(function () {
+	
+})
+forever(function () {
+    info.setScore(HeroCameraXOffset)
+})
+forever(function () {
+    scene.centerCameraAt(Hero.x + HeroCameraXOffset, Hero.y)
+})
