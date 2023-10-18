28f35d2ede9d391eadfca3e97c2b7d5bc8066d62
DLN-001 <77555673+DLN-001@users.noreply.github.com>
2023-10-18 00:03:58 -0500

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -4,12 +4,6 @@
 * Read more at https://arcade.makecode.com/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 
 /**
  * Custom blocks
@@ -17,25 +11,6 @@ enum MyEnum {
 //*https://fontawesome.com/v4/icons/
 //% weight=100 color=#0fbc11 icon="\uf0e7"
 namespace custom {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
 
     /**
      * Returns a single frame from an image array.
